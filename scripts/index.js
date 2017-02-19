f6282399f07718bf858f4ad5b91832e43db36614
Rakeem Thomas <thomas.rakeem.29@gmail.com>
2017-02-19 10:51:27 -0500

    clean up index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,29 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  const subHeadingArray = ['Web Developer', 'JavaScript Enthusiast', 'Learner'];
+const scrollSpyFunc = () => {
+  $('body').scrollspy({target: ".navbar", offset: 60});
+  $("#myNav a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function () {
+        window.location.hash = hash;
+      });
+    } 
+  });
+}
+
+const subHeadingFunc = () => {
+  const subHeadingArray = ['JavaScript Enthusiast', 'Life Learner', 'Web Developer'];
   const subHeadingEl = document.getElementById('subheading');
   let index = 0;
-    setInterval(function () {
+    setInterval(() => {
       subHeadingEl.textContent = subHeadingArray[index];
       index = (index + 1) % subHeadingArray.length;
     }, 3500);
-})
+}
 
-$('body').scrollspy({target: ".navbar", offset: 50});
-
-$("#myNav a").on('click', function(event) {
-
-  if (this.hash !== "") {
-
-    event.preventDefault();
-
-    var hash = this.hash;
-
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 900, function(){
-      window.location.hash = hash;
-    });
-  } 
+document.addEventListener('DOMContentLoaded', (event) => {
+  scrollSpyFunc();
+  subHeadingFunc();
 });
