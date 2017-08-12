2f579786240ef35c24128aee4e72cc4d47e84439
Rakeem Thomas <thomas.rakeem.29@gmail.com>
2017-08-12 19:07:01 -0400

    completed base layout

--- client/portfolio/src/About.js
+++ client/portfolio/src/About.js
@@ -4,13 +4,17 @@ import './App.css';
 
 function About() {
   return(
-    <div className="App">
-      <img src={Rakeem} alt="rakeem-profile-pic" />
-      <ul className="Social-Links">
-        <li><a href="https://twitter.com/rthom4s">Twitter</a></li>
-        <li><a href="https://www.linkedin.com/in/rakeem-thomas-b3295999/">LinkedIn</a></li>
-        <li><a href="https://github.com/rthomas29">Github</a></li>
-      </ul>
+    <div>
+      <div className="About-Div">
+        <img src={Rakeem} alt="rakeem-profile-pic" />
+      </div>
+      <div className="Social-Div">
+        <ul className="Social-Links">
+          <li><a href="https://twitter.com/rthom4s">Twitter</a></li>
+          <li><a href="https://www.linkedin.com/in/rakeem-thomas-b3295999/">LinkedIn</a></li>
+          <li><a href="https://github.com/rthomas29">Github</a></li>
+        </ul>
+      </div>
     </div>
   );
 }
